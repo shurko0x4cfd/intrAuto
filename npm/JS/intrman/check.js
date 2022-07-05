2a32463122864f04d2399acb267829907a1494d4
Shurko <shurko0x4cfd@gmail.com>
2022-07-06 00:02:00 +0300

    Начал добавлеть регулярки для проверки

--- npm/JS/intrman/check.js
+++ npm/JS/intrman/check.js
@@ -1,24 +1,30 @@
 // Проверяет файлы на отсутствие недопустимых выражений и наличие обязательных
-var check, for_pack, join, join_re_lists, obligate_for_private, obligate_for_public, wrongs_for_pack, wrongs_for_private, wrongs_for_public, wrongsearcher;
+var check, for_pack, join, join_re_lists, obligate_for_private, obligate_for_public, obligate_searcher, obligate_temp, wrongs_for_pack, wrongs_for_private, wrongs_for_public, wrongsearcher;
 
 import {
   read_line_while
 } from './auxiliary.js';
 
 import {
+  u,
+  cl,
   first
 } from 'raffinade';
 
-wrongs_for_pack = ['yadro\.introvert\.bz'];
+wrongs_for_pack = ['yadro\\.introvert\\.bz'];
 
-wrongs_for_public = ['yadroWidget', 'YadroWidget', 'yadroFunctions', 'this\.code\s*='];
+wrongs_for_public = ['yadroWidget', 'YadroWidget', 'yadroFunctions', 'this\\.code\\s*='];
 
-obligate_for_public = ['this\.version\s*=', 'this\.intr_code\s*='];
+obligate_for_public = ['this\\.version =', 'this\\.intr_code =', 'settings\\s*\\(.*\\)+\\s*{+|settings:\\s*\\(.*\\)+\\s*=>\\s*{+', 'bind_actions', 'onSave', 'init\\s*\\(.*\\)\\s*{+|init:\\s*\\(.*\\)\\s*=>\\s*{+'];
 
 wrongs_for_private = ['something'];
 
-obligate_for_private = ['this\.code\s*='];
+obligate_for_private = ['this\\.code\\s*='];
 
+obligate_temp = ['settings\\(\.\*\\)+'];
+
+// obligate_for_all = \
+//     [ ]
 for_pack = async function(file, publicity) {
   return (await check(file, publicity, wrongs_for_pack));
 };
@@ -31,15 +37,42 @@ check = async function(file, publicity, wrongspec) {
     wrongs = wrongs_for_private;
   }
   wrongs = join_re_lists(wrongs, wrongspec);
-  if (!wrongs) {
-    return;
+  //    if not wrongs
+  //        return
+  wrongs = (await read_line_while(file, wrongsearcher.bind(null, wrongs)));
+  if (wrongs) {
+    return wrongs;
   }
-  return (await read_line_while(file, wrongsearcher.bind(null, wrongs)));
+  return (await read_line_while(file, obligate_searcher.bind(null, obligate_for_public)));
 };
 
 wrongsearcher = function(regex, line) {
   var match;
   match = line.match(regex);
+  if (!match) {
+    return {
+      cond: true
+    };
+  }
+  match = first(match);
+  return {
+    cond: false,
+    val: match
+  };
+};
+
+obligate_searcher = function(regex_set, line) {
+  var i, len, match, regex;
+  for (i = 0, len = regex_set.length; i < len; i++) {
+    regex = regex_set[i];
+    match = line.match(regex);
+    if (match) {
+      cl(match);
+    }
+  }
+  return {
+    cond: true
+  };
   if (!match) {
     return {
       cond: true
--- npm/src/CoffeeScript/intrman/check.coffee
+++ npm/src/CoffeeScript/intrman/check.coffee
@@ -2,27 +2,38 @@
 # Проверяет файлы на отсутствие недопустимых выражений и наличие обязательных
 
 import { read_line_while } from './auxiliary.js'
-import { first } from 'raffinade'
+import { u, cl, first } from 'raffinade'
 
 
-wrongs_for_pack = ['yadro\.introvert\.bz']
+wrongs_for_pack = ['yadro\\.introvert\\.bz']
 
 wrongs_for_public = \
     [
         'yadroWidget',
         'YadroWidget',
         'yadroFunctions',
-        'this\.code\s*='
+        'this\\.code\\s*='
     ]
 
 obligate_for_public = \
     [
-        'this\.version\s*=',
-        'this\.intr_code\s*='
+        'this\\.version =',
+        'this\\.intr_code =',
+
+        'settings\\s*\\(.*\\)+\\s*{+|settings:\\s*\\(.*\\)+\\s*=>\\s*{+',
+        'bind_actions',
+        'onSave',
+        'init\\s*\\(.*\\)\\s*{+|init:\\s*\\(.*\\)\\s*=>\\s*{+'
     ]
 
 wrongs_for_private = ['something']
-obligate_for_private = ['this\.code\s*=',]
+obligate_for_private = ['this\\.code\\s*=']
+
+obligate_temp = ['settings\\(\.\*\\)+']
+
+
+# obligate_for_all = \
+#     [ ]
 
 
 for_pack = (file, publicity) ->
@@ -38,14 +49,34 @@ check = (file, publicity, wrongspec) ->
 
     wrongs = join_re_lists wrongs, wrongspec
 
-    if not wrongs
-        return
+#    if not wrongs
+#        return
     
-    await read_line_while file, wrongsearcher .bind null, wrongs
+    wrongs = await read_line_while file, wrongsearcher .bind null, wrongs
+
+    if wrongs
+        return wrongs
+
+    await read_line_while file, obligate_searcher .bind null, obligate_for_public
 
 
 wrongsearcher = (regex, line) ->
     match = line .match regex
+
+    if not match
+        return cond: true
+
+    match = first match
+
+    return cond: false, val: match
+
+
+obligate_searcher = (regex_set, line) ->
+    for regex in regex_set
+        match = line .match regex 
+        if match
+            cl match
+    return cond: true
 
     if not match
         return cond: true
